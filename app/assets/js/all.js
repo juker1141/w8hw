c726fa8d3185ecadda73d8eb2b1cd86278588595
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-06-18 00:12:25 +0800

    finish_first

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  //layout
   $(".navbar_menuBtn").click(function (e) {
     e.preventDefault();
     $(".navbar_menu-active").toggleClass('active');
@@ -8,6 +9,8 @@ $(document).ready(() => {
     e.preventDefault();
     $(".dropdown_menu").toggleClass('active');
   });
+
+  //index
   $(".checkBlockBtn").click(function (e) {
     e.preventDefault();
     $(".datepicker").toggleClass('active');
@@ -20,6 +23,8 @@ $(document).ready(() => {
     e.preventDefault();
     $(".guestsBlock").toggleClass('active');
   });
+
+  //result
   $(".sortBtn").click(function (e) {
     e.preventDefault();
     $(".sortList").toggleClass('active');
@@ -27,15 +32,20 @@ $(document).ready(() => {
   $(".filterBlockShowBtn").click(function (e) {
     e.preventDefault();
     $(".filterBlock").toggleClass('active');
+    $(".filterBlock").toggleClass('zIndexMax');
   });
   $(".filterBlockCloseBtn").click(function (e) {
     e.preventDefault();
     $(".filterBlock").toggleClass('active');
+    $(".filterBlock").toggleClass('zIndexMax');
   });
   $(".filterBlockDoneBtn").click(function (e) {
     e.preventDefault();
     $(".filterBlock").toggleClass('active');
+    $(".filterBlock").toggleClass('zIndexMax');
   });
+
+  //roomdetail
   $(".roomTypeBtn").click(function (e) {
     e.preventDefault();
     $(".orderBlock").toggleClass('active');
@@ -44,13 +54,20 @@ $(document).ready(() => {
     e.preventDefault();
     $(".orderBlock").toggleClass('active');
   });
+
+  //reserve
   $(".reserveDetailListBtn").click(function (e) {
     e.preventDefault();
     $(".reserveDetailList").toggleClass('active');
     $(".reserveDetailListHeadPrice").toggleClass('active');
+    $(".expandMoreBtn").toggleClass('active');
+    $(".expandLessBtn").toggleClass('active');
     $(".reserveDetailListHead").toggleClass('justify-content-between');
     $(".reserveDetailListHead").toggleClass('justify-content-end');
   });
+
+
+  //更改不同頁面的layout
   switch ($('title').text()) {
     case "Result":
       $('.resultLayoutHeader').addClass('loaded');
@@ -87,11 +104,14 @@ $(document).ready(() => {
       break;
     case "Member":
       $('.memberPageHide').addClass('d_none');
+      $('.memberPageShow').addClass('d-flex');
       $(".jumbotron").hide();
       break;
   }
 });
 
+
+//swiper預設
 const mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
   loop: true,
